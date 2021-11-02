811ae05ae09d4ee53fb407e39a7edc5eabb0491a
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-11-02 12:27:26 -0400

    lecture work-in-progress

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import DragonList from './components/DragonList';
 import './styles.css';
 
 const store = createStore(reducer);
-
+console.log(store);
 function App() {
   return (
     <div className="App">
