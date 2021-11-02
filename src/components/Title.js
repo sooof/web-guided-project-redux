efd2e9ad6264d1d858a81ab9d2fe0f8dcf4586fa
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-11-02 12:41:10 -0400

    lecture work-in-progress

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,5 +1,7 @@
 import React, { useState, useReducer } from 'react';
 
+import { connect } from 'react-redux';
+
 import titleReducer, { initialState } from '../reducers/titleReducer';
 import { toggleEditing, updateTitle} from './../actions/titleActions';
 
