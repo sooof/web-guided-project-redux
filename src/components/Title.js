42ab80d82b5e6bae2854e387253cc61789a6e24c
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-11-02 13:36:49 -0400

    lecture work-in-progress

--- src/components/Title.js
+++ src/components/Title.js
@@ -28,6 +28,14 @@ const mapStateToProps = (state) => {
 
 // export default Title;
 export default connect(mapStateToProps)(Title);
+
+// const connect = (mapStateToProps)=> {
+//   const newProps = mapStateToProps(state);
+//   return (Component) => {
+//     return <Component {...newProps}/>
+//   }
+// }
+
 // mapStateToProps : a function that gets the current state and injects into the props of our component anything returned.
 // mapActionsToProps : an object that contains action creator functions and injects those functions into props. Any function executed here is AUTOMATICALLY DISPATCHED.
 // component : the component we are injecting props into.
