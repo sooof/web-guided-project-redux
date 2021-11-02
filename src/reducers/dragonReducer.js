9867518c9095f28bcf6ca9540985a540570ca9fc
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-11-02 13:43:41 -0400

    lecture work-in-progress

--- src/reducers/dragonReducer.js
+++ src/reducers/dragonReducer.js
@@ -1,2 +1,8 @@
 //1. InitialState
+//   - add members to initialState
+
 //2. reducer function (list of our actions)
+//  - make reducer function with state and action as arguments.
+//  - add in addMember as an action case
+//  - default case that returns state
+//  - export out our reducer
